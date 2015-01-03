dae45d69c5902a297d076e08b527e4abadb2949e
Shahdee <13thshadow@gmail.com>
2015-01-03 19:41:19 +0300

    The book is accomplished 11 Chapter - DONE -Added nightvision effect

---
+++ Assets/Scripts/NightVision.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NightVision : MonoBehaviour {
+
+    public Shader m_Shader;
+
+    public float m_Contrast = 2.0f;
+    public float m_Brightness = 1f;
+    public Color m_NightVisColor = Color.white;
+    public Texture m_Vignette;
+    public Texture m_Stripes;
+    public float m_StripesTileAmount = 4.0f;
+    public Texture m_Noise;
+    public float m_NoiseXSpeed = 100;
+    public float m_NoiseYSpeed = 100;
+    public float m_Distortion = 0.2f;
+    public float m_Scale = 0.8f;
+
+    float m_Random = 0f;
+
+    Material _Material;
+    public Material Material 
+    {
+        get 
+        {
+            if (_Material == null) 
+                _Material = new Material(m_Shader);
+            return _Material;
+        }
+    }
+	
+	void Start () {
+
+        if (!SystemInfo.supportsImageEffects)
+            enabled = false;
+
+        if (!m_Shader && !m_Shader.isSupported)
+            enabled = false;
+	}
+	
+	
+	void Update () {
+
+        m_Contrast = Mathf.Clamp(m_Contrast, 0f, 4f);
+        m_Brightness = Mathf.Clamp(m_Brightness, 0f, 2f);
+        m_Random = Random.Range(-1f,1f);
+        m_Distortion = Mathf.Clamp(m_Distortion, -1f, 1f);
+        m_Scale = Mathf.Clamp(m_Scale, 0f,3f);
+
+	}
+
+    void OnRenderImage(RenderTexture source, RenderTexture dest) 
+    {
+        if (m_Shader != null)
+        {
+            Material.SetFloat("_Contrast", m_Contrast);
+            Material.SetFloat("_Brightness", m_Brightness);
+            Material.SetColor("_NightVisColor", m_NightVisColor);
+            Material.SetFloat("_Random", m_Random);
+            Material.SetFloat("_Distortion", m_Distortion);
+            Material.SetFloat("_Scale", m_Scale);
+
+            if (m_Vignette) 
+            {
+                Material.SetTexture("_Vignette", m_Vignette);
+            }
+
+            if (m_Stripes)
+            {
+                Material.SetTexture("_Stripes", m_Stripes);
+                Material.SetFloat("_StripesTileAmount", m_StripesTileAmount);
+            }
+
+            if (m_Noise) 
+            {
+                Material.SetTexture("_Noise", m_Noise);
+                Material.SetFloat("_NoiseXSpeed", m_NoiseXSpeed);
+                Material.SetFloat("_NoiseySpeed", m_NoiseYSpeed);
+            }
+            Graphics.Blit(source, dest, Material);
+        }
+        else
+            Graphics.Blit(source, dest, null);
+    }
+
+    void OnDestroy() 
+    {
+        if (Material != null)
+            DestroyImmediate(Material);
+    }
+}
---
+++ Assets/Shaders/11NightVision.shader
@@ -0,0 +1,79 @@
+﻿Shader "Custom/11NightVision" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_Contrast ("_Contrast", Range(0,4)) = 2
+		_Brightness ("_Brightness", Range(0,2)) = 1
+		_NightVisColor ("_NightVisColor", Color) = (1,1,1,1)
+		_Vignette ("_Vignette", 2D) = "white" {}
+		_Stripes ("_Stripes", 2D) = "white" {}
+		_StripesTileAmount ("_StripesTileAmount", Float) = 4.0
+		_Noise ("_Noise", 2D) = "white" {}
+		_NoiseXSpeed ("_NoiseXSpeed", Float) = 100.0
+		_NoiseYSpeed ("_NoiseYSpeed", Float) = 100.0
+		_Distortion ("_Distortion", Float) = 0.2 
+		_Scale ("_Scale", Float) = 0.8 
+		_Random ("_Random", Float) = 0 
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+
+		Pass{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#pragma fragmentoption ARB_preciosion_hint_fastest
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			uniform sampler2D _Vignette;
+			uniform sampler2D _Stripes;
+			uniform sampler2D _Noise;
+			fixed4 _NightVisColor;
+			fixed _Contrast;
+			fixed _StripesTileAmount;
+			fixed _Brightness;
+			fixed _Random;
+			fixed _NoiseXSpeed;
+			fixed _NoiseYSpeed;
+			fixed _Distortion;
+			fixed _Scale;
+
+			struct Input {
+				float2 uv_MainTex;
+			};
+
+			float2 BarrelDist(float2 coord)
+			{
+				float2 h = coord.xy - float2(0.5, 0.5);
+				float r2 = h.x * h.x + h.y * h.y;
+				float f = 1.0 + r2 * (_Distortion * sqrt(r2));
+				return f*_Scale*h + 0.5;
+			}
+
+			fixed4 frag(v2f_img i):COLOR
+			{
+				half2 distUV = BarrelDist(i.uv);
+				fixed4 renderTex = tex2D(_MainTex, distUV);
+				fixed4 vignetteTex = tex2D(_Vignette, i.uv);
+
+				half2 stripesUV = half2(i.uv.x * _StripesTileAmount, i.uv.y * _StripesTileAmount);
+				fixed4 stripesTex = tex2D(_Stripes, stripesUV);
+
+				half2 noiseUV = half2(i.uv.x + (_Random * _SinTime.z * _NoiseXSpeed), i.uv.y + (_Time.z * _NoiseYSpeed));
+				fixed4 noiseTex = tex2D(_Noise, noiseUV);
+
+				fixed lum = dot(fixed3(0.3, 0.6f, 0.1f), renderTex.rgb);
+				lum += _Brightness;
+				fixed4 finalCol = (lum*2) + _NightVisColor;
+
+				finalCol = pow(finalCol, _Contrast);
+				finalCol *= vignetteTex;
+				finalCol *= stripesTex*noiseTex;
+				return finalCol;
+			}
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
